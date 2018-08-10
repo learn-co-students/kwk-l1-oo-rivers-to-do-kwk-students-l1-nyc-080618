3ceded03c416daa94534b2dadb307aaf61b95ad9
graciemzhang <gracie.zhang@lps-students.org>
2018-08-10 14:46:25 +0000

    Done.

--- river.rb
+++ river.rb
@@ -1,1 +1,63 @@
 # river.rb
+
+class River 
+  def initialize(name)
+    @name = name
+  end
+  
+  def name 
+    return @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def length=(new_length)
+    @length = new_length
+  end
+  
+  def length
+    return @length
+  end
+  
+  def countries
+    return @countries
+  end
+  
+  def countries=(new_country)
+    @countries = new_country
+  end
+  
+  def discharge
+    return @discharge
+  end
+  
+  def discharge=(new_discharge)
+    @discharge = new_discharge
+  end
+  
+  def flood 
+    @discharge = @discharge * 0.3 + @discharge
+  end
+  
+  def dry_up
+    @discharge = @discharge - @discharge * 0.5 
+  end
+  
+end
+
+nile = River.new("The Nile")
+miss = River.new("The Mississippi")
+amazon = River.new("The Amazon")
+seine = River.new("The Seine")
+yangtze = River.new("The Yangtze")
+euphrates = River.new("The Euphrates")
+
+amazon.discharge=(1000)
+yangtze.discharge=(2030)
+euphrates.discharge=(20394390)
+
+amazon.flood
+yangtze.flood
+euphrates.dry_up
